640dcab21114b3be5b5730904c3ac74df8e3498d
Guilherme Castelao <guilherme@castelao.net>
2012-07-04 15:59:14 -0300

    Fixing authors and install_requires @ setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'numpy>=1.1',
+    'PyYAML',
 ]
 
 
@@ -23,8 +25,8 @@ setup(name='cnv',
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
     keywords='parser oceanography data',
-    author='Guilherme Castel\xc3\xa3o',
-    author_email='guilherme@castelao.net',
+    author='Guilherme Castel\xc3\xa3o , Roberto de Almeida, Luiz Irber',
+    author_email='guilherme@castelao.net,roberto@dealmeida.net, luiz.irber@gmail.com',
     url='',
     license='PSF',
     packages=find_packages('src'),
