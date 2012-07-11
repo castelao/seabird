21adc18070cfadaa91d97e837ac224d4f8508836
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2012-07-11 16:48:45 -0300

    Just the filenames to don't forget the concept: checks.py and netcdf.py

---
+++ src/cnv/checks.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Do a sequence of checks on the parsed CNV data
+"""
+
---
+++ src/cnv/netcdf.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Export the parced data into a NetCDF following different patterns
+"""
+
