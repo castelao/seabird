a27751e2a262c71df8a885601d1de4bf1093c49c
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2012-07-12 15:05:05 -0300

    Updating version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.1'
+version = '0.1.1'
 
 install_requires = [
     'numpy>=1.1',
@@ -33,7 +33,8 @@ setup(name='cnv',
     url='https://bitbucket.org/castelao/pycnv/wiki',
     license='PSF',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
+    package_dir = {'': 'src'},
+    include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
     entry_points={
