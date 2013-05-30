2341402a874e1b5f8e72ef82189adcfd6b390fa9
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-05-31 00:59:07 +0200

    Upgrade to 0.3, created cnv2nc to convert into netCDF4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.2.4'
+version = '0.3.0'
 
 install_requires = [
     'numpy>=1.1',
@@ -45,5 +45,5 @@ setup(name='cnv',
             ['cnv=cnv:main']
     },
     platforms='any',
-    scripts=["bin/cnvdump"],
+    scripts=["bin/cnvdump", "bin/cnv2nc"],
 )
