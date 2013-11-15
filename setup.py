4e5f9e23d5b0750407c3e2185f599412e882dfb5
Guilherme Castelao <guilherme@castelao.net>
2013-11-15 18:59:16 -0200

    Adding support to tests

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 import sys, os
+
+# ============================================================================
+from setuptools.command.test import test as TestCommand
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+# ============================================================================
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.4.5'
+version = '0.5.0'
 
 install_requires = [
     'numpy>=1.1',
@@ -45,4 +61,6 @@ setup(name='seabird',
     },
     platforms='any',
     scripts=["bin/cnvdump", "bin/cnv2nc"],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 )
