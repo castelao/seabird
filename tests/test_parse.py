5212ce18c5673f7a5afb9374df5ab0d9cbba88d4
Guilherme Castelao <guilherme@castelao.net>
2013-12-01 06:23:46 -0200

    Improving test_parse

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,10 +1,16 @@
+""" Parse some references with fCNV
+
+"""
+
 import os
 from glob import glob
 
-from cnv import cnv
+from seabird import cnv
 
-
-def test():
-    datadir = os.path.join(os.path.dirname(__file__), 'test_data')
-    for f in glob(os.path.join(datadir, "*.cnv.OK")):
-        profile = cnv.fCNV(f)
+def test_answer():
+        """ Load different .cnv versions with fCNV
+        """
+        datadir = os.path.join(os.path.dirname(__file__), 'test_data')
+        for f in glob(os.path.join(datadir, "*.cnv.OK")):
+            print "Loading file: %s" % f
+            profile = cnv.fCNV(f)
