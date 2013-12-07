343fadeff0b52d43f5ba1653b552abfe3ca3d4da
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-12-07 14:42:34 -0200

    Minor upgrade.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.5.0'
+version = '0.5.1'
 
 install_requires = [
     'numpy>=1.1',
