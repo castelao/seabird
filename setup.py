cc05a9d314da3998d6c36289b1181ac01335a5bc
Guilherme Castelao <guilherme@castelao.net>
2015-01-27 19:55:13 -0800

    Bugfix #16, ignore non UTF-8 to create netCDF.
    Thanks to Nathaniel Buck for the bug report.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.5.4'
+version = '0.5.5'
 
 install_requires = [
     'numpy>=1.1',
