f427f5a5617039abc97cc9332aa60a4cefac51a0
Gui <guilherme@castelao.net>
2015-03-02 16:48:42 -0800

    Prototype on Error Handling.
    The goal is to pass forward details of the error for the higher packages
    using this one, like CoTeDe.

--- src/seabird/__init__.py
+++ src/seabird/__init__.py
@@ -1,3 +1,9 @@
 #from cnv import *
 
 #__all__ = ['CNV', 'fCNV']
+
+class CNVError(Exception):
+    """Base class for exceptions in this module."""
+    def __init__(self, tag, msg):
+        self.tag = tag
+        self.msg = msg
