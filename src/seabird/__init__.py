5446bb6147c0d9f05bf49e61d603104dfe8fd56b
Gui <guilherme@castelao.net>
2015-03-25 10:25:56 -0700

    Default message for CNVError.

--- src/seabird/__init__.py
+++ src/seabird/__init__.py
@@ -4,6 +4,10 @@
 
 class CNVError(Exception):
     """Base class for exceptions in this module."""
-    def __init__(self, tag, msg):
+    def __init__(self, tag, msg=None):
         self.tag = tag
-        self.msg = msg
+
+        if msg is not None:
+            self.msg = msg
+        elif tag == 'noparsingrule':
+            self.msg = "There are no rules able to parse the input."
