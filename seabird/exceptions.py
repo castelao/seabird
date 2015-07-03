e630f744739c93ee66b191b249a635b6037c37e1
Gui <guilherme@castelao.net>
2015-07-02 19:07:18 -0700

    Moving CNVError into seabird.exceptions

---
+++ seabird/exceptions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class CNVError(Exception):
+    """Base class for exceptions in this module."""
+    def __init__(self, tag, msg=None):
+        self.tag = tag
+
+        if msg is not None:
+            self.msg = msg
+        elif tag == 'noparsingrule':
+            self.msg = "There are no rules able to parse the input."
