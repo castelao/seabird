99ff36374ef46692e4f83b7b1ce84d84357b70ba
Gui <guilherme@castelao.net>
2015-07-26 15:39:49 -0700

    Removing import exceptions from __init__.py

--- seabird/__init__.py
+++ seabird/__init__.py
@@ -2,10 +2,8 @@
 
 __author__ = 'Guilherme Castelao'
 __email__ = 'guilherme@castelao.net'
-__version__ = '0.6.0'
+__version__ = '0.6.2'
 
 #from cnv import *
 
 #__all__ = ['CNV', 'fCNV']
-
-from exceptions import *
