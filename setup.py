68133b73a12b2159d99e0bd50898d2e2efaaf34d
Guilherme Castelao <guilherme@castelao.net>
2016-11-13 20:49:06 -0800

    Compatible with py3.4 -> py3.5

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering',
     ],
     entry_points={
