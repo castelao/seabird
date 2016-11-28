47645de91796d24e3ffe4a13b8e26b71a6dee460
Guilherme Castelao <guilherme@castelao.net>
2016-11-27 20:40:14 -0800

    Moving and improving parsing test.

---
+++ seabird/test/test_parse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Parse some references with fCNV
+
+"""
+
+import os.path
+from glob import glob
+
+from seabird.cnv import fCNV
+from seabird.utils import seabird_dir
+
+
+def test_answer():
+        """ Load different .cnv versions with fCNV
+        """
+        datafiles = glob(os.path.join(seabird_dir(), 'data', "*.cnv"))
+        assert len(datafiles) > 0, \
+            "No files available for testing at: %s" % datafiles
+        for f in datafiles:
+            print("Loading file: %s" % f)
+            profile = fCNV(f)
+            assert len(profile.keys()) > 0
+            assert len(profile.attributes.keys()) > 0
