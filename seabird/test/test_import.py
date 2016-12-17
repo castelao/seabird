f456b2e5ee1a89fc835c0a351a9d274e918f3cf9
Guilherme Castelao <guilherme@castelao.net>
2016-12-17 10:12:26 -0800

    Moving test_import and simplifying it.

---
+++ seabird/test/test_import.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def test_ImportShortcut():
+    """ Test shortcut to import CNV & fCNV
+
+        CNV & fCNV are actually inside seabird.cnv, but to simplify
+          I placed a shortcut.
+    """
+    from seabird import CNV
+    from seabird import fCNV
