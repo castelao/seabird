3188e993ccbd8ae49c43f21ccb35947364030bcd
Guilherme Castelao <guilherme@castelao.net>
2016-12-17 10:13:06 -0800

    Test all rules but refnames.

--- seabird/test/test_rules.py
+++ seabird/test/test_rules.py
@@ -18,7 +18,7 @@ def test_load_available_rules():
     """
     rules_dir = 'rules'
     rule_files = pkg_resources.resource_listdir(seabird.__name__, rules_dir)
-    rule_files = [f for f in rule_files if re.match('^cnv.*json$', f)]
+    rule_files = [f for f in rule_files if re.match('^(?!refnames).*json$', f)]
     for rule_file in rule_files:
         print("loading rule: %s", (rule_file))
         text = pkg_resources.resource_string(
