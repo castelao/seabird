6a38e8e954e71abd10aa091120078fc258843b6f
Guilherme Castelão <guilherme@castelao.net>
2017-03-05 14:17:20 -0800

    New test for blank note line. BUG #37, #40.
    Temporary solution. Must be improved.

--- seabird/test/test_parse.py
+++ seabird/test/test_parse.py
@@ -23,3 +23,13 @@ def test_answer():
             profile = fCNV(f)
             assert len(profile.keys()) > 0
             assert len(profile.attributes.keys()) > 0
+
+def test_blank_note_line():
+    """ Temporary solution to avoid #37 & #40
+
+        Guarantee that issues #37 & #40 will not repeat. Maybe change this
+          to test with load_rule instead?
+    """
+    blank_note_line = "* Sea-Bird SBE9  Data File:\n*\n* \n# name 3 = depSM\n# file_type = ascii\n*END*\n1\n"
+    from seabird.utils import load_rule
+    load_rule(blank_note_line)
