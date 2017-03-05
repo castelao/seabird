d350fad40883dbcfa2e3e2af8f617f3fcca73635
Guilherme Castelão <guilherme@castelao.net>
2017-03-05 14:40:02 -0800

    Some dummy data for testing.

---
+++ seabird/test/dummydata.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+minimalist = "* Sea-Bird SBE9  Data File:\n* \n# name 3 = depSM\n# file_type = ascii\n*END*\n1\n"
+
+blank_note_line = "* Sea-Bird SBE9  Data File:\n*\n* \n# name 3 = depSM\n# file_type = ascii\n*END*\n1\n"
