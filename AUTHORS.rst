92fa2c829027173b8e418ab1e58637fe439be30b
Guilherme Castelão <guilherme@castelao.net>
2019-08-20 22:41:05 -0700

    BUGFIX #53, skipping first data line if without headers
    Thanks to @mbiddle-bcodmo for reporting this BUG.

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -15,7 +15,8 @@ Contributors
 * Rohan Dandavati; Provided the CTD-bottle example file: MI18MHDR.btl
 * Suki Wong; Reported bug #42 on cnv2nc.
 * Paul Focke; Position base splitting #45.
-* Amber York; Reporting bug $41 on reading bottle files.
+* Amber York; Reported bug #41 on reading bottle files.
 * Achim Randelhoff; minor improvements in documentation
 * Uday Bhaskar; Provided SK287_CTD05.cnv, data columns with headers.
                 Reported bad path on seabird_dir() for windows machines.
+* Mathew Biddle; Reported bug #53 on skipping first line if without header.
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -8,7 +8,7 @@
 import os.path
 from glob import glob
 
-from seabird.cnv import fCNV
+from seabird.cnv import CNV, fCNV
 from seabird.utils import seabird_dir
 
 
@@ -33,3 +33,27 @@ def test_blank_note_line():
     blank_note_line = "* Sea-Bird SBE9  Data File:\n*\n* \n# name 3 = depSM\n# file_type = ascii\n*END*\n1\n"
     from seabird.utils import load_rule
     load_rule(blank_note_line)
+
+
+def test_column_header():
+    """Parse with or without the data columns headers
+
+       The data columns usually have a header, like:
+       ...
+       # file_type = ascii
+       *END*
+       Depth      Press
+       3.973      3.995
+       ...
+
+       Parse the data even without the column header (Depth & Press).
+    """
+    raw = "* Sea-Bird SBE 9 Data File:\n* System UpLoad Time = Aug 01 2011 11:34:32\n# nquan = 2\n# nvalues = 3\n# name 0 = depSM: Depth [salt water, m]\n# name 1 = prDM: Pressure, Digiquartz [db]\n# start_time = Aug 01 2011 11:34:32\n# bad_flag = -9.990e-29\n# datcnv_date = Aug 02 2011 04:16:47, 7.18c\n# file_type = ascii\n*END*\n     Depth      Press  \n      3.973      3.995\n      4.079      4.102\n      3.902      3.924\n"
+    profile = CNV(raw)
+    assert len(profile['DEPTH']) == 3
+    assert profile['DEPTH'][0] == 3.973
+
+    # Now without the headers
+    profile = CNV(raw.replace('     Depth      Press  \n' ,''))
+    assert len(profile['DEPTH']) == 3
+    assert profile['DEPTH'][0] == 3.973
