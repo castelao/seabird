41757d3c6bd03ddb4747951a2464fdab053b2613
Guilherme Castelão <guilherme@castelao.net>
2019-08-22 23:09:28 -0700

    Bump version: 0.11.1 → 0.11.2

--- seabird/__init__.py
+++ seabird/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Guilherme Castelao'
 __email__ = 'guilherme@castelao.net'
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 
 from .cnv import CNV, fCNV
 from .exceptions import CNVError
