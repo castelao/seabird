f265117fa14901d63042d3b01e443fc180ac76e9
Guilherme Castelao <guilherme@castelao.net>
2023-01-24 21:53:54 -0700

    Updating sampledata()
    It looks like I never added these changes into the repository. Let's see
    if it works.

--- seabird/utils.py
+++ seabird/utils.py
@@ -108,32 +108,73 @@ def seabird_dir(subdir=None):
     return os.path.expanduser(spath).replace('/', os.path.sep)
 
 
-def sampledata():
+def sampledata(filename=None, dtype=None):
+    """Return the full path to local sample data
+
+       The first time it will download the sample data files into the default
+         seabird directory. Check seabird_dir() if you want to modify that.
+    """
     try:
-        import supportdata
-    except:
-        print("Missing package supportdata. Try:\npip install supportdata")
+        from supportdata import download_file
+    except ImportError:
+        module_logger.error(
+                "Missing package supportdata. Try:\npip install supportdata")
 
-    data_path = os.path.join(seabird_dir(), 'data')
-    if not os.path.isdir(data_path):
-        os.makedirs(data_path)
+    outputdir = os.path.join(seabird_dir(), 'sampledata')
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
 
-    src = 'https://raw.githubusercontent.com/castelao/seabird/dev/sampledata'
-    files = [
-            ['CTD', 'PIRA001.cnv', '5ded777144300b63c8775b1d7f033f92'],
-            ['CTD', 'dPIRX003.cnv', '4b941b902a3aea7d99e1cf4c78c51877'],
-            ['CTD', 'Hotin.cnv', '814dc769c0775327bbe5b0f489dfb571'],
-            ['CTD', 'sta0860.cnv', '1c788c4d9b82b527ebf0c2fb9200600e'],
-            ['CTD', 'missing_whitespace.cnv',
-                'c1f00cebb5f00f6aaebc316bac3fd86a'],
-            #['CTD', 'laurynas.cnv', '6f188d53ac2d7aaaf4ce69c0e5c514ec'],
-            ['TSG', 'TSG_PIR_001.cnv', '2950ccb9f77e0802557b011c63d2e39b'],
-            ['TSG', 'TSG_PIR_010.cnv', 'd87cea33bfe37e22dc8e563f77cbf307'],
-            ['btl', 'MI18MHDR.btl', '775f2a6c6585f1cffb0038111580e5a1'],
-            ]
+    src = "https://raw.githubusercontent.com/castelao/seabird/dev/sampledata"
 
-    for f in files:
-        supportdata.download_file(
-                os.path.join(data_path, f[0]),
-                os.path.join(src, f[0], f[1]),
-                f[1], f[2])
+    filesdb = {
+        'PIRA001.cnv': {
+            'dtype': "CTD", 'md5': "5ded777144300b63c8775b1d7f033f92"},
+        'dPIRX003.cnv': {
+            'dtype': "CTD", 'md5': "4b941b902a3aea7d99e1cf4c78c51877"},
+        'dPIRX010.cnv': {
+            'dtype': "CTD", 'md5': "8691409accb534c83c8bd412afbdd285"},
+        'Hotin.cnv': {
+            'dtype': "CTD", 'md5': "814dc769c0775327bbe5b0f489dfb571"},
+        'missing_whitespace.cnv': {
+            'dtype': "CTD", 'md5': "c1f00cebb5f00f6aaebc316bac3fd86a"},
+        'SK287_CTD05.cnv': {
+            'dtype': "CTD", 'md5': "08e974c46ed603442eecf9145031a6c4"},
+        'sta0860.cnv': {
+            'dtype': "CTD", 'md5': "1c788c4d9b82b527ebf0c2fb9200600e"},
+        'more_after_file_type.cnv': {
+            'dtype': "CTD", 'md5': "e5bffcfdcaf52333773bbe7abe98b06d"},
+        # ['CTD', 'laurynas.cnv', '6f188d53ac2d7aaaf4ce69c0e5c514ec'],
+        'TSG_PIR_001.cnv': {
+            'dtype': "TSG", 'md5': "2950ccb9f77e0802557b011c63d2e39b"},
+        'TSG_PIR_010.cnv': {
+            'dtype': "TSG", 'md5': "d87cea33bfe37e22dc8e563f77cbf307"},
+        # 'MI18MHDR.btl': {
+        #     'dtype': "btl", 'md5': "775f2a6c6585f1cffb0038111580e5a1"},
+        }
+
+    if dtype is not None:
+        for f in [f for f in filesdb if filesdb[f]['dtype'] != dtype]:
+            del(filesdb[f])
+
+    if filename is None:
+        datafile = []
+        for f in filesdb:
+            print(f)
+            download_file(outputdir=outputdir,
+                          url=os.path.join(src, filesdb[f]["dtype"], f),
+                          filename=f,
+                          md5hash=filesdb[f]["md5"])
+            datafile.append(os.path.join(outputdir, f))
+        return datafile
+
+    elif filename in filesdb:
+        download_file(outputdir=outputdir,
+                      url=os.path.join(src,
+                                       filesdb[filename]["dtype"],
+                                       filename),
+                      filename=filename,
+                      md5hash=filesdb[filename]["md5"])
+        return os.path.join(outputdir, filename)
+
+    else:
+        module_logger.error("%s is not a valid test file" % filename)
