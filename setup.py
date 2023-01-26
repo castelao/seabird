8b859251caa52d4c1a3c8cbce5622c0b087031a5
Guilherme Castelao <guilherme@castelao.net>
2023-01-25 21:31:17 -0700

    fix: Missing a version

--- setup.py
+++ setup.py
@@ -23,9 +23,6 @@ with open('requirements_dev.txt', encoding='utf-8') as f:
     requirements_test = f.read()
 
 setup(
-    name='seabird',
-    version='0.11.5',
-    description="Parser for Sea-Bird's CTD and TSG.",
     long_description=readme + '\n\n' + history,
     author='Guilherme Castelao , Luiz Irber',
     author_email='guilherme@castelao.net, luiz.irber@gmail.com',
@@ -36,24 +33,8 @@ setup(
     package_dir={'seabird':
                  'seabird'},
     include_package_data=True,
-    install_requires=requirements,
     license='3-clause BSD',
     zip_safe=False,
-    keywords='oceanography ocean data CTD TSG SeaBird hydrography parser',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering',
-    ],
     entry_points={
         'console_scripts':
             ['seabird=seabird.cli:cli']
